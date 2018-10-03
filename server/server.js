c756133f37de84d524bf1673cf04a516485f0e95
nasir-abbas <nasir.abbas@blueeast.com>
2018-10-03 23:04:34 +0500

    create server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
